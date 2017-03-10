d445867383c3b5660cb9acba55b7b68f7c48e545
Hashan <hashangod@gmail.com>
2017-03-09 19:11:12 -0500

    Progress so far

---
+++ controllers/pushnotifications.js
@@ -0,0 +1,27 @@
+var apn = require('apn');
+
+var apnProvider = new apn.Provider({
+  token: {
+    key: 'apns.p8', // Path to the key p8 file
+    keyId: 'ABCDE12345', // The Key ID of the p8 file (available at https://developer.apple.com/account/ios/certificate/key)
+    teamId: 'ABCDE12345', // The Team ID of your Apple Developer Account (available at https://developer.apple.com/account/#/membership/)
+  },
+  production: false // Set to true if sending a notification to a production iOS app
+});
+
+var self = module.exports = {
+  var deviceToken = 'xxxxxxxxxxxxxxxxxx'; // Dvice token
+  var notification = new apn.Notification();
+  notification.topic = 'my.bundle.id'; // iOS app's Bundle ID
+  notification.expiry = Math.floor(Date.now() / 1000) + 3600;
+  notification.badge = 3;
+  notification.sound = 'ping.aiff';
+  notification.alert = 'Hello World \u270C';
+
+  sendPushNotification: function(id) {
+    notification.payload = {videoClipId: id};
+    apnProvider.send(notification, deviceToken).then(function(result) {
+      console.log(result);
+    });
+  }
+}
---
+++ routes/api.js
@@ -0,0 +1,48 @@
+/**
+* Copyright 2016, Google, Inc.
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+var pushnotifications = require('./controllers/pushnotifications');
+
+router.get('/', function(req, res) {
+  res.json({ message: 'hooray! welcome to our api!' });
+});
+
+router.get('/getVideoClip/:clip_id', (req, res) => {
+  res.json({ message: 'This api will return the video for id: ' + req.params.clip_id });
+});
+
+router.post('/uploadVideoClip', function(req, res) {
+  if (!req || !req.files.videoClip || !req.files)
+  return res.status(400).send('No file was uploaded.');
+
+  let uploadedFile = req.files.videoClip;
+
+  // TODO: Give a different name to each clip with an ID appended
+  uploadedFile.mv('./video_clips/clip2.jpg', function(err) {
+    if (err)
+    return res.status(500).send(err);
+
+    // send push notification with the id of the video clip
+    //pushnotifications.sendPushNotification(id)
+
+    res.send('File uploaded!');
+  });
+});
+
+module.exports = router;
